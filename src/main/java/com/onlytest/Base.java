88813d5be896f15e82b9518b3a9946d17a5e7d9d
Akki2222 <123179616+Akki2222@users.noreply.github.com>
2025-07-06 02:27:53 +0530

    Update Base.java

--- src/main/java/com/onlytest/Base.java
+++ src/main/java/com/onlytest/Base.java
@@ -1,6 +1,7 @@
 package com.onlytest;
 
 import java.time.Duration;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
